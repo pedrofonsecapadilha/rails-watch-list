fb4ad81a5c2150fcba2db97e4bcba0ee55667e4e
Pedro Padilha <pedrofpadilha@gmail.com>
2022-11-17 16:36:08 -0300

    Challenge2 - Falta frontEnd

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,22 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params.require(:list).permit(:name))
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists do
+    resources :bookmarks
+  end
+
+  resources :bookmarks
 end
